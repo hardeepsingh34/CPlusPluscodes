d07cf59e8d5e7d7392067ba0a9135914de5baad3
hardeepsingh34 <hardeepbhatti325.com>
2024-09-05 20:29:32 +0530

    binary tree height diameter

---
+++ programd170.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class node{
+    public:
+    int data;
+    node * left ;
+    node* right;
+    node(int d){
+        this->data= d;
+        this->left = NULL;
+        this->right=NULL;
+    }
+};
+class solution{
+    public:
+    // function to find the height of a binary tree
+    int height(node* Node){
+        // base case
+        if(Node == NULL){
+            return 0;
+        }
+        int left = height(Node->left);
+        int right = height(Node-> right);
+
+        int ans = max(left, right)+1;
+        return ans;
+    }
+};
+void buildFromLevelOrder(node * &root){
+    queue<node *>q;
+    cout<<"Enter data for root "<<endl;
+    int data;
+    cin>>data;
+    root = new node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        cout<<"Enter left node for : "<<temp->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata!= -1){
+            temp->left= new node(leftdata);
+            q.push(temp->left);
+        }
+        cout<<"Enter right node for : "<<temp->data<<endl;
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata != -1){
+            temp->right = new node(rightdata);
+            q.push(temp->right);
+        }
+    }
+
+}
+int main(){
+node * root = NULL;
+solution s;
+buildFromLevelOrder(root);
+cout<<"Height of given tree is : "<<s.height(root)<<endl;
+
+}
---
+++ programd171.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d){
+        this->data= d;
+        this->left= NULL;
+        this->right = NULL;
+    }
+};
+class solution{
+public:
+int height(Node * node){
+// base case
+if(node == NULL){
+    return 0;
+}
+int left= height(node-> left);
+int right = height(node-> right);
+int ans= max(left, right)+1;
+return ans;
+}
+int diameter(Node* root){
+    // base case
+    if(root == NULL){
+        return 0;
+    }
+    int op1= diameter(root->left);
+    int op2= diameter(root->right);
+    int op3= height(root ->left)+height(root->right)+1;
+
+    int ans= max(op1, max(op2, op3));
+    return ans;
+}
+};
+void buildFromLevelOrder(Node * &root){
+    queue<Node *>q;
+    cout<<"Enter data for root "<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        cout<<"Enter left node for : "<<temp->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata!= -1){
+            temp->left= new Node(leftdata);
+            q.push(temp->left);
+        }
+        cout<<"Enter right node for : "<<temp->data<<endl;
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata != -1){
+            temp->right = new Node(rightdata);
+            q.push(temp->right);
+        }
+    }
+
+}
+int main(){
+Node* root;
+solution s;
+buildFromLevelOrder(root);
+cout<<"diameter of given tree is : "<<s.diameter(root)<<endl;
+
+}
